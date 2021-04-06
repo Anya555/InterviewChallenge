73dc755f0e5ed2b59aead99e53bb906408448ad9
Anna Panas <annapanas0906@gmail.com>
2021-04-06 01:12:43 -0400

    adding improved solutions

---
+++ debugging/index.js
@@ -0,0 +1,19 @@
+// define a constructor function
+function Banana(diameter, length) {
+  this.color = "yellow";
+  this.diameter = diameter;
+  this.length = length;
+  this.yummy = true;
+  // add a rot method to constructor
+  this.rot = function () {
+    console.log("all bananas can rot");
+  };
+}
+// create new objects from constructor
+let banana1 = new Banana(1, 2);
+console.log(banana1);
+banana1.rot();
+
+let banana2 = new Banana(3, 5);
+console.log(banana2);
+banana2.rot();
---
+++ mongoDB/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
+const app = express();
+const db = require("./models");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose
+  .connect("mongodb://localhost:27017/books", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the Database successfully");
+  });
+
+// insert some data
+db.Book.create([
+  {
+    name: "Harry Potter",
+  },
+  {
+    name: "Lord of the Rings",
+  },
+  {
+    name: "Long Bright River",
+  },
+]);
+
+// Start an app an click on "Click here to see books" link in browser to get a list of books
+app.get("/api/books", async (req, res) => {
+  try {
+    const books = await db.Book.find({});
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
